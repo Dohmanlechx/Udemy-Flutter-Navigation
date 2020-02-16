8591a3fceeeecfa281caac0a9a76e42a21783573
David Öhman <dohman_92@hotmail.com>
2020-02-16 20:59:54 +0100

    -

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../data/dummy_data.dart';
+import 'items/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        children: <Widget>[
+          ...DUMMY_CATEGORIES
+              .map((it) => CategoryItem(
+                    it.title,
+                    it.color,
+                  ))
+              .toList()
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
